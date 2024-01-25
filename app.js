2c634eba11f3bb9b9fa02b1a71c152e7a5cb34c3
Indhiraraj <indhiraraj7@gmail.com>
2024-01-25 22:33:26 +0530

    modify app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"))
 
-app.set("views", "/var/task/views");
+app.set("views", "./views");
 
 
 app.get("/", async function (req, res) {
