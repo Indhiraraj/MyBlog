0f2e5a3b695521a7904028006dc47e4b018b22c2
Indhiraraj <indhiraraj7@gmail.com>
2024-01-25 22:08:50 +0530

    add index.js

---
+++ app.js
@@ -0,0 +1,98 @@
+import express from "express";
+
+import bodyParser from "body-parser";
+
+import _ from "lodash";
+
+// import getPosts from "./getAllPosts.js";
+
+
+
+const app = express();
+
+
+
+let posts = [];
+
+
+
+
+
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"))
+
+app.get("/", async function (req, res) {
+
+    const getPostsModule = import("./getAllPosts.js");
+
+getPostsModule.then(async ({ default: getPosts }) => {
+  // Now you can use getPosts as you would normally
+  const postsJson = await getPosts();
+  posts = JSON.parse(postsJson);
+}).catch(error => {
+  // Handle any potential errors during import
+  posts=[];
+  console.error("Error importing getPosts module:", error);
+  
+});
+
+
+   
+
+    //res.render("header",{pageTitle: title});
+
+    res.render("home", { posts: posts })
+})
+
+
+app.post("/", function (req, res) {
+
+})
+
+app.get("/posts/:id", function (req, res) {
+
+    posts.forEach(element => {
+        
+        if (_.lowerCase(element.title) === _.lowerCase(req.params.id)) {
+            res.render("post", { title: element.title, body: element.content })
+        } else {
+            res.render("post", { title: "SRY...", body: "No posts" })
+        }
+
+    });
+
+
+})
+
+
+
+app.get("/compose", function (req, res) {
+    res.render("compose")
+})
+
+app.post("/compose", function (req, res) {
+
+    const post = {
+        "title": req.body.postTitle,
+        "content": req.body.postBody
+    }
+
+    res.redirect("/")
+
+})
+
+app.get("/about", function (req, res) {
+    res.render("about")
+})
+
+app.get("/contacts", function (req, res) {
+    res.render("contacts")
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server starts at port 3000")
+})
